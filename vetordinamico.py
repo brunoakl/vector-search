bb9edd9f256570e852cc762f74f6114afb469b4f
brunoakl <64101776+brunoakl@users.noreply.github.com>
2023-08-16 16:19:11 -0400

    Funções melhoradas
    A estrutura que organiza os campos e botões na tela foi ajustada para mais dinamismo com a janela.
    Botões agora centralizam de acordo com a dimensão da janela(tamanho base é 500x500).
    Botão "Remover" implementado. Agora pode remover apenas o nome do usuário do vetor ou todo o par de dados, caso fornecidos.
    Mudanças mínimas no método remove da classe VetorDinamico e comentários para melhor entendimento.

--- vetordinamico.py
+++ vetordinamico.py
@@ -7,17 +7,20 @@ class VetorDinamico:
         """ Ordena o vetor em ordem crescente """
         self.dados[:self.tamanho] = sorted(self.dados[:self.tamanho])
 
+    #Inicializa o vetor
     def __init__(self, capacidade_inicial=1):
         self.capacidade = capacidade_inicial
         self.tamanho = 0
         self.dados = [None] * capacidade_inicial
 
+    
     def append(self, valor, nome=None):
         if self.tamanho == self.capacidade:
             self._redimensionar(2 * self.capacidade)
         self.dados[self.tamanho] = (valor, nome)
         self.tamanho += 1
 
+    #Muda o tamanho do vetor de acordo com a necessidade
     def _redimensionar(self, nova_capacidade):
         nova_lista = [None] * nova_capacidade
         for i in range(self.tamanho):
@@ -32,23 +35,31 @@ class VetorDinamico:
                 self.dados[i] = (valor, nome)
                 return
         raise ValueError(f"Valor {valor} não encontrado no vetor.")
-
-    def __str__(self):
-        return ', '.join(f"{num}({nome})" if nome else str(num) for num, nome in self.dados[:self.tamanho])
     
-    def remove(self, valor):
-        indice = -1
-        for i in range(self.tamanho):
-            if self.dados[i] == valor:
-                indice = i
-                break
-        if indice == -1:
-            raise ValueError(f"Valor {valor} não encontrado no vetor.")
+    def remove(self, valor=None, nome=None):
+        if valor is None and nome is None:
+            messagebox.showerror("Erro","Forneça um número e/ou nome para remover.")
+            return
         
-        for i in range(indice, self.tamanho-1):
-            self.dados[i] = self.dados[i+1]
-        self.tamanho -= 1
+        if valor is None and nome:
+            for i in range(self.tamanho):
+                num, nome_atual = self.dados[i]
+                if nome_atual == nome:
+                    self.dados[i] = (num, None)
+                    return
 
+        i = 0
+        while i < self.tamanho:
+            num, nome_atual = self.dados[i]
+            if ((valor is not None and num == valor) or
+                (nome is not None and nome_atual == nome)):
+                for j in range(i, self.tamanho-1):
+                    self.dados[j] = self.dados[j+1]
+                self.tamanho -= 1
+            else:
+                i += 1
+
+    #Junta nome e número no vetor
     def __str__(self):
         return ', '.join(map(str, self.dados[:self.tamanho]))
 
@@ -56,33 +67,56 @@ class App:
     def __init__(self, root):
         self.vetor = VetorDinamico()
 
-        self.lblId = tk.Label(root, text="ID:")
-        self.lblId.pack(pady=10, side=tk.LEFT)
-        self.entryNumber = tk.Entry(root)
-        self.entryNumber.pack(pady=10)
+        #Organizando campos de texto
+        self.inputFrame = tk.Frame(root)
+        self.inputFrame.grid(row=0, column=0, sticky="nsew", pady=20, padx=100)
+        
+        root.grid_rowconfigure(0, weight=1)  # Permite que a linha 0 se estique conforme necessário
+        root.grid_columnconfigure(0, weight=1)
+        
+        #Campos de entrada e seus labels
+        self.lblId = tk.Label(self.inputFrame, text="ID:")
+        self.lblId.grid(row=0, column=0, sticky=tk.E, padx=(0,5))
+        
+        self.entryNumber = tk.Entry(self.inputFrame, width=20)
+        self.entryNumber.grid(row=0, column=1, sticky=tk.W)
 
-        self.lblName = tk.Label(root, text="Usuário:")
-        self.lblName.pack(pady=10, side=tk.LEFT)
-        self.entryName = tk.Entry(root)
-        self.entryName.pack(pady=10)
-
+        self.lblName = tk.Label(self.inputFrame, text="Usuário:")
+        self.lblName.grid(row=1, column=0, sticky=tk.E, padx=(0,5))
+        
+        self.entryName = tk.Entry(self.inputFrame, width=20)
+        self.entryName.grid(row=1, column=1, sticky=tk.W)
+        
+        
+        #Botões
+        #Adiciona ao vetor
         self.btnAdd = tk.Button(root, text="Adicionar Número", command=self.adicionar)
-        self.btnAdd.pack()
+        self.btnAdd.grid(row=1, column=0, pady=5, padx=10, columnspan=2)
 
+        #Vincula o nome ao número indicado do vetor
         self.btnLink = tk.Button(root, text="Vincular Nome", command=self.vincular)
-        self.btnLink.pack()
-
-       
-
+        self.btnLink.grid(row=2, column=0, pady=5, padx=10, columnspan=2)
+        
+        #Adiciona 5 a 15 números ao vetor, de 1 a 100
         self.btnRandom = tk.Button(root, text="Gerar Vetor Aleatório", command=self.gerar_aleatorio)
-        self.btnRandom.pack(pady=10)
+        self.btnRandom.grid(row=3, column=0, pady=5, padx=10, columnspan=2)
 
+        #Ordena o vetor a partir dos números, em ordem crescente
         self.btnSort = tk.Button(root, text="Ordenar", command=self.ordenar)
-        self.btnSort.pack(pady=10)
+        self.btnSort.grid(row=4, column=0, pady=5, padx=10, columnspan=2)
+        
+        #Remove dados do vetor
+        self.btnRemove = tk.Button(root, text="Remover", command=self.remover)
+        self.btnRemove.grid(row=5, column=0, pady=5, padx=10, columnspan=2)
 
-        self.output = tk.Label(root, text="", font=("Arial", 12))
-        self.output.pack(pady=20)
+        #Widget que exibe o vetor
+        self.output = tk.Text(root, height=10, width=50, font=("Arial", 12))
+        self.scrollbar = tk.Scrollbar(root, command=self.output.yview)
+        self.output.config(yscrollcommand=self.scrollbar.set)
+        self.output.grid(row=6, column=0, pady=20, padx=10, columnspan=2)
 
+    #Funções de manipulação do vetor
+    #Autoexplicativas
     def adicionar(self):
         try:
             valor = int(self.entryNumber.get())  # Corrigido aqui
@@ -90,9 +124,22 @@ class App:
             self.update_display()
         except ValueError:
             messagebox.showerror("Erro", "Por favor, insira um número válido.")
-
     
-
+    def remover(self):
+        valor = self.entryNumber.get()
+        nome = self.entryName.get()
+        
+        if not valor and not nome:
+            messagebox.showerror("Erro","Forneça dados para remoção")
+            return
+        
+        try:
+            int_valor = int(valor) if valor else None
+            str_nome = nome if nome else None
+            self.vetor.remove(int_valor, str_nome)
+            self.update_display()
+        except ValueError as e:
+            messagebox.showerror("Erro", str(e))
     
     def gerar_aleatorio(self):
         tamanho = random.randint(5, 15)  # Gera um tamanho aleatório entre 5 e 15
@@ -116,13 +163,15 @@ class App:
         except ValueError:
             messagebox.showerror("Erro", f"Valor {valor} não encontrado no vetor.")
 
-
+    #Atualiza o widget do vetor com os dados que foram adicionados ou apagados
     def update_display(self):
-        self.output['text'] = str(self.vetor)
+        self.output.delete(1.0, tk.END)  # Limpar o widget Text
+        self.output.insert(tk.END, str(self.vetor))
 
     
-
+#Especificações da janela do app
 root = tk.Tk()
 root.title("Vetor Dinâmico")
+root.geometry("500x500")
 app = App(root)
 root.mainloop()
